e8bcb734ae5fa659d7ca2aafa5836ddf61f025e6
Osman <korogluosman85@gmail.com>
2025-10-29 18:20:48 +0300

    Discount area prepared for API

--- QRMenuWebUI/WiewComponents/DefaultComponents/_DefaultOfferComponentPartial.cs
+++ QRMenuWebUI/WiewComponents/DefaultComponents/_DefaultOfferComponentPartial.cs
@@ -1,12 +1,34 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using QRMenuWebUI.Dtos.DiscountDtos;
+using QRMenuWebUI.Dtos.SliderDtos;
+using System.Net.Http;
 
 namespace QRMenuWebUI.WiewComponents.DefaultComponents
 {
     public class _DefaultOfferComponentPartial:ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DefaultOfferComponentPartial(IHttpClientFactory httpClientFactory)
         {
-            return View();
+            _httpClientFactory = httpClientFactory;
+        }
+
+        // Invoke() yerine InvokeAsync() kullanın
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44366/api/Discount");
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultDiscountDto>>(jsonData);
+                return View(values ?? new List<ResultDiscountDto>());
+            }
+
+            return View(new List<ResultSliderDtos>());
         }
     }
     
