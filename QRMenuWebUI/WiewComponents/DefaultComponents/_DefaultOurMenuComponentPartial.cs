8684a0e9740ce5b8d7cf714fb11acf3ce537815e
Osman <korogluosman85@gmail.com>
2025-10-29 20:54:54 +0300

    Products were added to the menu

--- QRMenuWebUI/WiewComponents/DefaultComponents/_DefaultOurMenuComponentPartial.cs
+++ QRMenuWebUI/WiewComponents/DefaultComponents/_DefaultOurMenuComponentPartial.cs
@@ -1,13 +1,34 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Newtonsoft.Json;
+using QRMenuWebUI.Dtos.ProductDtos;
+using QRMenuWebUI.Dtos.SliderDtos;
 
 namespace QRMenuWebUI.WiewComponents.DefaultComponents
 {
     public class _DefaultOurMenuComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DefaultOurMenuComponentPartial(IHttpClientFactory httpClientFactory)
         {
-            return View();
+            _httpClientFactory = httpClientFactory;
+        }
+
+        // Invoke() yerine InvokeAsync() kullanın
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44366/api/Product");
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultProductDto>>(jsonData);
+                return View(values ?? new List<ResultProductDto>());
+            }
+
+            return View(new List<ResultSliderDtos>());
         }
     }
 }
