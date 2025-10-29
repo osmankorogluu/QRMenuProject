baf3158c6557b09b3b736331c7e021412c26e091
Osman <korogluosman85@gmail.com>
2025-10-29 16:43:29 +0300

    UI design fix2

---
+++ QRMenuWebUI/WiewComponents/DefaultComponents/_DefaultOurMenuComponentPartial.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace QRMenuWebUI.WiewComponents.DefaultComponents
+{
+    public class _DefaultOurMenuComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
+    
+
---
+++ QRMenuWebUI/WiewComponents/DefaultComponents/_DefaultTestimonialComponentPartial.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QRMenuWebUI.WiewComponents.DefaultComponents
+{
+    public class _DefaultTestimonialComponentPartial:ViewComponent  
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+    
+    }
+
