6529991e76ac348469fd0950addd654842541d78
Osman <korogluosman85@gmail.com>
2025-10-29 15:35:08 +0300

    UI design fix

---
+++ QRMenuWebUI/WiewComponents/DefaultComponents/_DefaultOfferComponentPartial.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QRMenuWebUI.WiewComponents.DefaultComponents
+{
+    public class _DefaultOfferComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+    
+    }
+
